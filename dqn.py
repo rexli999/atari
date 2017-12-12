c23ea6ac03dffdd379db45555b592e1e41862b46
Zeyuan Jin <zeyuan.1.jin@gmail.com>
2017-12-12 00:38:47 -0500

    Update dqn.py

--- dqn.py
+++ dqn.py
@@ -152,15 +152,15 @@ def learn(env,
     gather_indicies = tf.range(batch_size) * tf.shape(current_q_func)[1] + act_t_ph
     q_act_value = tf.gather(tf.reshape(current_q_func, [-1]), gather_indicies)
 
-    if if_double:
-        next_q_func = q_func(obs_tp1_float, num_actions, scope = "q_func", reuse = False)
-    else:
-        next_q_func = target_q_func
+#     if if_double:
+#         next_q_func = q_func(obs_tp1_float, num_actions, scope = "q_func", reuse = False)
+#     else:
+#         next_q_func = target_q_func
 
-    best_actions = tf.cast(tf.argmax(next_q_func, axis = 1), tf.int32)
-    gather_indicies_target = tf.range(batch_size) * tf.shape(target_q_func)[1] + best_actions
-    target_q_value = tf.gather(tf.reshape(target_q_func, [-1]), gather_indicies_target)
-    q_act_estimate = rew_t_ph + gamma * target_q_value  * (1 - done_mask_ph)#tf.reduce_max(target_q_func, reduction_indices=[1])
+#     best_actions = tf.cast(tf.argmax(next_q_func, axis = 1), tf.int32)
+#     gather_indicies_target = tf.range(batch_size) * tf.shape(target_q_func)[1] + best_actions
+#     target_q_value = tf.gather(tf.reshape(target_q_func, [-1]), gather_indicies_target)
+    q_act_estimate = rew_t_ph + gamma * (1 - done_mask_ph) * tf.reduce_max(target_q_func, reduction_indices=[1])
 
     total_error = huber_loss(tf.subtract(q_act_estimate, q_act_value))
     loss_summary = tf.summary.scalar('Huber_Loss', total_error)
