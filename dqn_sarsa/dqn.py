1e9d96800942a9343895df018cf6a9b3c0b813e6
XinLuoCU <xl2614@columbia.edu>
2017-12-13 21:06:49 -0500

    ???

--- dqn_sarsa/dqn.py
+++ dqn_sarsa/dqn.py
@@ -162,7 +162,7 @@ def learn(env,
 
     actlist=[]
     for i in range(batch_size):
-        if random.random() < e is not None:
+        if tf.cond(random.random() < e):
             act = env.action_space.sample()
         else:
             input_batch = replay_buffer.encode_recent_observation()
